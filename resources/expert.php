971f32305ba9f1c596d35a126a3a35207a070359
shivamtg <shivamtg1376@gmail.com>
2022-06-17 14:51:10 +0200

    exercise 1

--- resources/expert.php
+++ resources/expert.php
@@ -2,5 +2,14 @@
 declare(strict_types=1);
 
 
+echo "Exercise 1 starts here:";
 
+function new_exercise($x) {
+    $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
+    echo $block;
+
+}
+
+
+new_exercise(2);
 
