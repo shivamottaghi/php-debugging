887deac1e658a916f0c334b96dc97dd03b617e00
shivamtg <shivamtg1376@gmail.com>
2022-06-17 14:52:09 +0200

    exercise 2,3

--- resources/expert.php
+++ resources/expert.php
@@ -13,3 +13,18 @@ function new_exercise($x) {
 
 new_exercise(2);
 
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+
+echo $monday;
+
+
+new_exercise(3);
+
+$str = "\"Debugged !\" Also very fun";
+echo substr($str, 0, 12);
+
+
+
+
+new_exercise(4);
