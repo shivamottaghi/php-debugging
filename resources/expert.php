c586851849ba58d36325c42fb81c602ce0d07dc2
shivamtg <shivamtg1376@gmail.com>
2022-06-20 10:46:09 +0200

    ex 10

--- resources/expert.php
+++ resources/expert.php
@@ -137,3 +137,16 @@ echo isLinkValid('http://google.com/test.txt');
 
 new_exercise(10);
 
+//Filter the array $areTheseFruits to only contain valid fruits
+//do not change the arrays itself
+$areTheseFruits = ['apple', 'bear', 'beef', 'banana', 'cherry', 'tomato', 'car'];
+$validFruits = ['apple', 'pear', 'banana', 'cherry', 'tomato'];
+//from here on you can change the code
+$count = count($areTheseFruits);
+for($i=0; $i < $count; $i++) {
+    if(!in_array($areTheseFruits[$i], $validFruits)) {
+        unset($areTheseFruits[$i]);
+    }
+}
+var_dump($areTheseFruits);//do not change this
+
