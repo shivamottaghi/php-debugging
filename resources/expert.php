01913cc94ac29ed40dd9d970d5c6f92617525ffe
shivamtg <shivamtg1376@gmail.com>
2022-06-20 09:35:01 +0200

    ex 8

--- resources/expert.php
+++ resources/expert.php
@@ -97,3 +97,21 @@ function copyright($year) {
 copyright(date('Y'));
 
 new_exercise(8);
+
+function login(string $email, string $password) {
+    if($email == 'john@example.be' && $password == 'pocahontas') {
+        return 'Welcome John' .' Smith' .'<br>';
+    }
+    return 'No access' .'<br>';
+}
+
+//do not change anything below
+//should great the user with his full name (John Smith)
+echo login('john@example.be', 'pocahontas') ;
+//no access
+echo login('john@example.be', 'dfgidfgdfg');
+//no access
+echo login('wrong@example.be', 'wrong');
+//you can change things again!
+
+new_exercise(9);
